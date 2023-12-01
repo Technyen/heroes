7222d4be4f524d27c1739d35bef34420a4d22f36
Technyen <penghe201211@gmail.com>
2023-12-01 17:04:49 -0300

    codeCfo

--- Backend/Infrastructure/Services/MarvelService.cs
+++ Backend/Infrastructure/Services/MarvelService.cs
@@ -16,7 +16,7 @@ namespace Infrastructure.Services
         public async Task<List<CreatorResult>> GetCreatorsAsync()
         {
 
-            var responseString = await _httpClient.GetStringAsync("creators?ts=1&apikey=a3b5b24ba6ba6a846a5e774e90e340b7&hash=6e94d8b4a9f883c9a1f732d5d4c9fd99");
+            var responseString = await _httpClient.GetStringAsync("");
             var root = JsonConvert.DeserializeObject<Root>(responseString);
             return root.Data.Results;
         }
